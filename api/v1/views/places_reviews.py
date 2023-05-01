ffb470a54b76cfeef1e8cd0af8ee0f4478ec50a3
Meacha1 <meachattd@gmail.com>
2023-05-01 11:14:18 -0700

    review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -73,8 +73,8 @@ def update_review(review_id):
         abort(400, 'Not a JSON')
     data = request.get_json()
     for key, value in data.items():
-        if key not in ['id', 'user_id', 'place_id',
-                       'created_at', 'updated_at']:
+        if key not in ['id', 'user_id',
+                       'place_id', 'created_at', 'updated_at']:
             setattr(review, key, value)
     storage.save()
     return jsonify(review.to_dict()), 200
