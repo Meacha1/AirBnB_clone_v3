c2ac3081b89a1e7d3e56c0ed5b83c654a91d6bc9
Meacha1 <meachattd@gmail.com>
2023-05-01 12:52:42 -0700

    p_amenities

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -7,48 +7,54 @@ from flask import jsonify, abort, request
 from models import storage
 from models.place import Place
 from models.amenity import Amenity
+from datetime import datetime
+import uuid
+from os import getenv
 
 
-@app_views.route('places/<place_id>/amenities', methods=['GET'])
-def get_amenities(place_id):
-    """Retrieves the list of all Amenity objects"""
-    place = storage.get(Place, place_id)
-    if place is None:
-        abort(404)
-    amenities_list = []
-    for amenity in place.amenities:
-        amenities_list.append(amenity.to_dict())
-    return jsonify(amenities_list)
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    @app_views.route('places/<place_id>/amenities',
+                     methods=['GET'])
+    def get_amenities(place_id):
+        """Retrieves the list of all Amenity objects"""
+        place = storage.get(Place, place_id)
+        if place is None:
+            abort(404)
+        amenities_list = []
+        for amenity in place.amenities:
+            amenities_list.append(amenity.to_dict())
+        return jsonify(amenities_list)
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE'])
-def delete_amenity(place_id, amenity_id):
-    """Deletes an Amenity object"""
-    place = storage.get(Place, place_id)
-    if place is None:
-        abort(404)
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity is None:
-        abort(404)
-    if place.amenities.count(amenity) == 0:
-        abort(404)
-    place.amenities.remove(amenity)
-    storage.save()
-    return jsonify(amenity.to_dict()), 200
-
-
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'])
-def create_amenity(place_id, amenity_id):
-    """Creates an Amenity object"""
-    place = storage.get(Place, place_id)
-    if place is None:
-        abort(404)
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity is None:
-        abort(404)
-    if place.amenities.count(amenity) != 0:
+    @app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                     methods=['DELETE'])
+    def delete_amenity(place_id, amenity_id):
+        """Deletes an Amenity object"""
+        place = storage.get(Place, place_id)
+        if place is None:
+            abort(404)
+        amenity = storage.get(Amenity, amenity_id)
+        if amenity is None:
+            abort(404)
+        if place.amenities.count(amenity) == 0:
+            abort(404)
+        place.amenities.delete(amenity)
+        storage.save()
         return jsonify(amenity.to_dict()), 200
-    place.amenities.append(amenity)
-    storage.save()
-    return jsonify(amenity.to_dict()), 201
+
+
+    @app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                     methods=['POST'])
+    def create_amenity(place_id, amenity_id):
+        """Creates an Amenity object"""
+        place = storage.get(Place, place_id)
+        if place is None:
+            abort(404)
+        amenity = storage.get(Amenity, amenity_id)
+        if amenity is None:
+            abort(404)
+        if place.amenities.count(amenity) != 0:
+            return jsonify(amenity.to_dict()), 200
+        place.amenities.append(amenity)
+        storage.save()
+        return jsonify(amenity.to_dict()), 201
